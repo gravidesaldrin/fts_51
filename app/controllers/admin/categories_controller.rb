6f1cafae8263d2709dc519b94bf8887bf77ee348
Aldrin <aldrin.s.gravides@framgia.com>
2016-03-30 17:35:16 +0800

    Add category masterfile files

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,38 @@
+class Admin::CategoriesController < ApplicationController
+  load_and_authorize_resource
+  before_action :authenticate_user!
+
+  def index
+    @categories = @categories.order(:name).page params[:page]
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t ".success"
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t ".success"
+      redirect_back_or admin_categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:success] = t ".success"
+    redirect_to admin_categories_path
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :id, :name, :image, :remove_image
+  end
+end
